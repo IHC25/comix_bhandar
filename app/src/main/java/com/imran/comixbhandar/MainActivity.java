b8f0837fba5f9dc1ece1b18cba69db20eb0b022a
IHC25 <imranhchy@gmail.com>
2024-10-13 11:39:50 +0600

    Initial commit

---
+++ app/src/main/java/com/imran/comixbhandar/MainActivity.java
@@ -0,0 +1,32 @@
+package com.imran.comixbhandar;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+public class MainActivity extends AppCompatActivity {
+
+    PDFView pdfView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        pdfView = findViewById(R.id.pdfView);
+
+        pdfView.fromAsset("soviet_deshe_tintin.pdf")
+                .load();
+    }
+}
